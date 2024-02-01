44763339e84374b15530239bee545ecfc3f8f5fe
Jean Osco <willjean29@gmail.com>
2024-01-31 22:02:50 -0500

    feat(task): add handler to upload images

--- main.go
+++ main.go
@@ -8,13 +8,54 @@ import (
 	"app/routes"
 	"encoding/json"
 	"errors"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
+func uploadFileHandler(w http.ResponseWriter, r *http.Request) {
+	// Parsea la solicitud
+	r.ParseMultipartForm(10 << 20) // 10mb limite
+
+	// Obtiene el archivo de la solicitud
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		json.NewEncoder(w).Encode(error.New("Error retrieving the file", http.StatusBadRequest, err))
+		return
+	}
+	defer file.Close()
+
+	// Crea la carpeta destino si no existe
+	err = os.MkdirAll("uploads", os.ModePerm)
+	if err != nil {
+		json.NewEncoder(w).Encode(error.New("Error creating the folder", http.StatusInternalServerError, err))
+		return
+	}
+
+	// Crea un nuevo archivo en el servidor
+	newFile, err := os.Create("uploads/" + handler.Filename)
+	if err != nil {
+		json.NewEncoder(w).Encode(error.New("Error creating the file", http.StatusInternalServerError, err))
+		return
+	}
+	defer newFile.Close()
+
+	// Copia el contenido del archivo subido al nuevo archivo
+	_, err = io.Copy(newFile, file)
+
+	if err != nil {
+		json.NewEncoder(w).Encode(error.New("Error copying the file", http.StatusInternalServerError, err))
+		return
+	}
+
+	// Envía una respuesta de éxito
+	json.NewEncoder(w).Encode(map[string]string{"file": handler.Filename, "message": "File uploaded successfully"})
+}
 func main() {
 	var defaultPort int64 = 4000
 	db.DBConnection()
@@ -22,15 +63,21 @@ func main() {
 	port := strconv.FormatInt(defaultPort, 10)
 	router := mux.NewRouter().StrictSlash(true)
 
+	fs := http.FileServer(http.Dir("./uploads/"))
+	router.PathPrefix("/uploads/").Handler(http.StripPrefix("/uploads/", fs))
+
 	router.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			log.Println(r.Method, r.URL.Path)
-			w.Header().Set("Content-Type", "application/json")
+			if !strings.Contains(r.URL.Path, "/uploads/") && r.Method == http.MethodGet {
+				w.Header().Set("Content-Type", "application/json")
+			}
 			next.ServeHTTP(w, r)
 		})
 	})
 
 	router.HandleFunc("/", handlers.IndexRoute)
+	router.HandleFunc("/upload", uploadFileHandler).Methods(http.MethodPost)
 	routes.TaskRoutes(router)
 
 	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
