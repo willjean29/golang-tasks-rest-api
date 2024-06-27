930685f9f4d03cdc2cf33493713e14bd0d7ce9ea
Jean Osco <willjean29@gmail.com>
2024-06-26 20:41:18 -0500

    feat(files): add file controller with gin

--- src/modules/files/infra/http/routes/files.routes_gin.go
+++ src/modules/files/infra/http/routes/files.routes_gin.go
@@ -1,18 +1,16 @@
 package routes
 
 import (
+	"app/src/modules/files/infra/http/controllers"
 	"app/src/shared/infra/http/middlewares"
 
 	"github.com/gin-gonic/gin"
 )
 
 func FileRoutesGin(router *gin.RouterGroup) {
+	var filesController = controllers.FileControllerGin{}
 	filesRoutes := router.Group("/files")
 	filesRoutes.Use(middlewares.AuthenticatedGin())
 
-	filesRoutes.POST("/:collection/:id", middlewares.UploadFileGin(), func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "File uploaded",
-		})
-	})
+	filesRoutes.POST("/:collection/:id", middlewares.UploadFileGin(), filesController.UploadFile)
 }
